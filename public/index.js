7c56ab59894fd44d8980b7a9d1a78fe9ad78ed59
Niaura <78849699+Niaura@users.noreply.github.com>
2022-03-22 15:23:16 +0100

    Ajout de MUI

--- public/index.js
+++ public/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 const container = document.getElementById('root');
 
-const MyComponent = () => {
+//const MyComponent = () => {
+//  return (
+//    <button variant ="contained">
+//      Bonjour
+//    </button>
+//  );
+//};
+
+function Test(){
   return (
-    <h2>This is my first component !!!</h2>
-  )
+    <Button variant ="contained">
+      Bonjour
+    </Button>
+  );
 };
 
-ReactDOM.render(<MyComponent />, container);
+
+//ATTENTION : SI SUR MUI VOUS AVEZ document.querySelector('#app') MODIFIEZ AVEC ROOT
+ReactDOM.render(<Test />, document.querySelector('#root'));
+
+
