c7816f5c2d22850292d5e5b3903d0d98f425872b
lillypuce <65114833+lillypuce@users.noreply.github.com>
2022-03-29 14:43:58 +0200

    Resolution erreur: lecture d'images possible

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 var app = express();
 
 app.use('/dist/js', express.static('./dist/js'));
+app.use('/images', express.static('./images'));
 
 app.get('/', (req, res) => {
 	res.sendFile('./index.html', { root : __dirname});
