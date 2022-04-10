e8f75a1c05e3add2b699766258f7868a2d108a63
jlaikine <78849823+jlaikine@users.noreply.github.com>
2022-04-10 16:40:43 +0200

    Ajout accueil.js

---
+++ public/accueil.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+
+export function ImageFond(){
+    return (
+        <picture>
+            <img
+                src={'/images/accueilbienvenu.jpg'}
+                alt="Albi accueil"
+                width="100%"
+            >
+            </img>
+        </picture>
+    );
+}
+
+export function Suggestion() {
+    const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'Musée Toulouse-Lautrec',
+    imgPath:
+      './images/museeT.jpg',
+  },
+  {
+    label: 'Le parc Rochegude',
+    imgPath:
+      './images/parc.jpg',
+  },
+  {
+    label: 'Restaurant Le Cascarbar',
+    imgPath:
+      './images/Rcascarbar.jpg',
+  },
+  {
+    label: 'Espace zoologique Exoticamis',
+    imgPath:
+      './images/Zoo.jpg',
+  },
+  {
+    label: 'Hôtel les Pasteliers',
+    imgPath:
+      './images/Hpasteliers.jpg',
+  },
+];
+
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 600, flexGrow: 1 }} b>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: '#b4b8c8',
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 350,
+                  display: 'block',
+                  maxWidth: 600,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="medium"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="medium" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+
+
+
