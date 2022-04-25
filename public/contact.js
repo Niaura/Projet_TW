aeb2140d6191487765e6f065b3985e52764bb900
jlaikine <78849823+jlaikine@users.noreply.github.com>
2022-04-25 17:16:41 +0200

    modif formulaire

--- public/contact.js
+++ public/contact.js
@@ -12,6 +12,11 @@ import Alert from "@mui/material/Alert";
 import IconButton from "@mui/material/IconButton";
 import Collapse from "@mui/material/Collapse";
 import CloseIcon from "@mui/icons-material/Close";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import CancelScheduleSendIcon from "@mui/icons-material/CancelScheduleSend";
+import ScheduleSendIcon from "@mui/icons-material/ScheduleSend";
+import SendIcon from "@mui/icons-material/Send";
 
 export function Formulaire() {
   const handleChange = (event) => {
@@ -22,11 +27,47 @@ export function Formulaire() {
   };
   const handleChange3 = (event) => {
     if (event.target.value === 100) {
-      setOpen(true);
+      if (
+        document.getElementById("nom").value != "" &&
+        document.getElementById("prenom").value != "" &&
+        document.getElementById("datedenaissance").value != "" &&
+        document.getElementById("adresse").value != "" &&
+        document.getElementById("telephone").value != "" &&
+        document.getElementById("codepostal").value != "" &&
+        document.getElementById("email").value != "" &&
+        document.getElementById("textmessage").value != ""
+      ) {
+        setOpen(true);
+      } else {
+        alert("Veuillez compléter les cases vides !");
+      }
     }
+  };
+  const handleChange4 = (event) => {
+    document.getElementById("coordonnee").style.visibility = "visible";
   };
   const [open, setOpen] = React.useState(false);
   const [slidevalue, setSlidevalue] = React.useState(0);
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange5 = () => {
+    setChecked((prev) => !prev);
+  };
+
+  const marks = [
+    {
+      value: 0,
+      label: <CancelScheduleSendIcon />
+    },
+    {
+      value: 50,
+      label: <ScheduleSendIcon />
+    },
+    {
+      value: 100,
+      label: <SendIcon />
+    }
+  ];
 
   const theme = createTheme({
     palette: {
@@ -42,72 +83,28 @@ export function Formulaire() {
   });
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Contactez-nous</h1>
-      <Grid container spacing={2}>
-        <Grid item xs={2} />
-        <Grid item xs={8}>
-          <div
-            style={{
-              border: "2px solid black",
-              padding: "7px",
-              borderRadius: "10px",
-              margin: "10px"
-            }}
-          >
-            <Grid container spacing={10} columns={{ xs: 4 }}>
-              <Grid item xs={1}>
-                <TextField
-                  id="Nom"
-                  label="Nom"
-                  variant="outlined"
-                  required="true"
-                  inputProps={{
-                    style: { fontFamily: "nunito", color: "black" }
-                  }}
-                />
-              </Grid>
-              <Grid item xs={1}>
-                <TextField
-                  id="Prénom"
-                  label="Prénom"
-                  variant="outlined"
-                  required="true"
-                  inputProps={{
-                    style: { fontFamily: "nunito", color: "black" }
-                  }}
-                />
-              </Grid>
-              <Grid item xs={1}>
-                <TextField
-                  id="E-mail"
-                  label="E-mail"
-                  variant="outlined"
-                  required="true"
-                  inputProps={{
-                    style: {
-                      fontFamily: "nunito",
-                      color: "black"
-                    }
-                  }}
-                />
-              </Grid>
-              <Grid item xs={1}>
-                <TextField
-                  id="Téléphone"
-                  label="Téléphone"
-                  variant="outlined"
-                  required="true"
-                  inputProps={{
-                    style: { fontFamily: "nunito", color: "black" }
-                  }}
-                />
-              </Grid>
-            </Grid>
-          </div>
-        </Grid>
-        <Grid item xs={2} />
-      </Grid>
-      <br />
+      <div
+        style={{
+          textAlign: "center",
+          backgroundColor: "#D3D3D3"
+        }}
+      >
+        <h1>--- Contactez-nous ---</h1>
+        <h3>
+          Que vous planifiiez un voyage de loisirs ou d’affaires, seul, en
+          famille ou en groupe, nous sommes ravis de vous accompagner dans
+          l’organisation de votre séjour à Albi !
+        </h3>
+        <p style={{ fontSize: "18px" }}>
+          On aime entretenir une relation personnalisée avec notre public. C'est
+          pourquoi nous vous proposons de choisir votre moyen de communication
+          privilégié.
+        </p>
+        <p style={{ fontSize: "18px" }}>
+          Contactez-nous pour toute demande d'informations et de suggestion.
+          Albi Tourisme vous aide à préparer votre séjour à Albi.
+        </p>
+      </div>
 
       <div style={{ textAlign: "center" }}>
         <FormControl id="jesuis" sx={{ m: 1, width: 600 }}>
@@ -160,16 +157,77 @@ export function Formulaire() {
       </div>
 
       <br />
+
       <div id="message" style={{ textAlign: "center", visibility: "hidden" }}>
+        <FormControl id="civilité" sx={{ m: 1, width: 600 }}>
+          <InputLabel id="civilité2">Civilité*</InputLabel>
+          <Select
+            id="civilité"
+            onChange={handleChange4}
+            autoWidth
+            label="Civilité*"
+          >
+            <MenuItem value="Madame">Madame</MenuItem>
+            <MenuItem value="Monsieur">Monsieur</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <br />
+      <div
+        id="coordonnee"
+        style={{ textAlign: "center", visibility: "hidden" }}
+      >
+        <FormControl id="civilité" sx={{ m: 1, width: 600 }}>
+          <TextField id="nom" label="Nom*" />
+
+          <br />
+
+          <TextField id="prenom" label="Prénom*" />
+
+          <br />
+
+          <TextField
+            id="datedenaissance"
+            label="Date de naisssance"
+            InputLabelProps={{ shrink: true, required: true }}
+            type="date"
+          />
+
+          <br />
+
+          <TextField id="adresse" label="Adresse*" />
+
+          <br />
+
+          <TextField id="codepostal" label="Code postal*" />
+
+          <br />
+
+          <TextField id="telephone" label="Téléphone*" />
+
+          <br />
+
+          <TextField id="email" label="E-mail*" />
+
+          <br />
+        </FormControl>
+
+        <br />
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextareaAutosize
+              id="textmessage"
               placeholder="Votre message"
               style={{ resize: "none", width: 600 }}
               minRows={15}
             ></TextareaAutosize>
           </Grid>
-
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Switch checked={checked} onChange={handleChange5} />}
+              label="J’accepte que mes informations soient enregistrées afin de rendre le service demandé"
+            />
+          </Grid>
           <Grid item xs={12}>
             <ThemeProvider theme={theme} id="themeprovider">
               <Slider
@@ -177,41 +235,43 @@ export function Formulaire() {
                 defaultValue={slidevalue}
                 color="primary"
                 onChange={handleChange3}
+                marks={marks}
                 sx={{
-                  width: 125,
+                  width: 175,
                   color: "success.dark",
                   "& .MuiSlider-thumb": {
-                    borderRadius: "1px"
+                    borderRadius: "6px"
                   }
                 }}
               />
             </ThemeProvider>
           </Grid>
         </Grid>
-      </div>
-      <br />
 
-      <div id="messageenvoye" style={{ width: "100%" }}>
-        <Collapse in={open}>
-          <Alert
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpen(false);
-                  window.location.reload();
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-            sx={{ mb: 2 }}
-          >
-            Message envoyé!
-          </Alert>
-        </Collapse>
+        <br />
+
+        <div id="messageenvoye" style={{ width: "100%" }}>
+          <Collapse in={open}>
+            <Alert
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setOpen(false);
+                    window.location.reload();
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+              sx={{ mb: 2 }}
+            >
+              Message envoyé!
+            </Alert>
+          </Collapse>
+        </div>
       </div>
     </>
   );
